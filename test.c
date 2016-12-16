08238d30a9af98388bd6a40e3b4630e9f9e9322b
Bernard Gautier <bernard.gautier4@gmail.com>
2016-12-16 10:35:27 +0100

    Point de départ du partiel.

--- test.c
+++ test.c
@@ -1,13 +1,17 @@
 #include <stdint.h>
 
-/* Base address for UART0 */
-#define UART0_BASE_ADDR ...
-/* Data register address for UART */
-#define UART0_DR ...
-/* Interrupt Mask set/clear register address for UART0 */
-#define UART0_IMSC ...
+#define UART0_BASE_ADDR 0x10009000
+#define UART0_DR (*((volatile uint32_t *)(UART0_BASE_ADDR + 0x000)))
+#define UART0_IMSC (*((volatile uint32_t *)(UART0_BASE_ADDR + 0x038)))
+#define UART0_MIS (*((volatile uint32_t *)(UART0_BASE_ADDR + 0x040)))
+#define UART0_ICR (*((volatile uint32_t *)(UART0_BASE_ADDR + 0x044)))
 
-/* Base address for GIC (Generic Interrupt Controller) and some registers */
+#define UART1_BASE_ADDR ...
+#define UART1_DR ...
+#define UART1_IMSC ...
+#define UART1_MIS ...
+#define UART1_ICR ...
+
 #define GIC1_BASE_ADDR 0x10040000
 #define GIC2_BASE_ADDR 0x10050000
 #define GICC_CTRL 				(*((volatile uint32_t *)(GIC1_BASE_ADDR + 0x0000)))
@@ -17,74 +21,69 @@
 #define GICD_ISENABLER1 		(*((volatile uint32_t *)(GIC1_BASE_ADDR + 0x1104)))
 #define GICD_IPTR				(*((volatile uint32_t *)(GIC1_BASE_ADDR + 0x182c)))
 
-/* UART interrupt (this is irq interrupt) */
-void __attribute__((interrupt)) ...()
-{
-	/* To be completed */
-	...
+void __attribute__((interrupt)) irq_handler() {
+
+	if (UART0_MIS ...) {
+		/* IRQ is coming from UART0 */
+		...;					/* Write data on UART0 TX */
+		UART0_ICR = ...;		/* Acknowledge RX interrupt from UART0 */
+	}
+
+	if (UART1_MIS ...) {
+		/* IRQ is coming from UART1 */
+		...;					/* Write data on UART1 TX */
+		UART1_ICR = ...;		/* Acknowledge RX interrupt from UART1 */
+	}
+
 }
 
 /* all other handlers are infinite loops */
-void __attribute__((interrupt)) undef_handler(void)
-{
+void __attribute__((interrupt)) undef_handler(void) {
 	for (;;)
 		;
 }
-void __attribute__((interrupt)) swi_handler(void)
-{
+void __attribute__((interrupt)) swi_handler(void) {
 	for (;;)
 		;
 }
-void __attribute__((interrupt)) prefetch_abort_handler(void)
-{
+void __attribute__((interrupt)) prefetch_abort_handler(void) {
 	for (;;)
 		;
 }
-void __attribute__((interrupt)) data_abort_handler(void)
-{
+void __attribute__((interrupt)) data_abort_handler(void) {
 	for (;;)
 		;
 }
-void __attribute__((interrupt)) fiq_handler(void)
-{
+void __attribute__((interrupt)) fiq_handler(void) {
 	for (;;)
 		;
 }
-/*
- * Copy interrupt vectors to address 0
- */
-void copy_vectors(void)
-{
+
+void copy_vectors(void) {
 	extern uint32_t vectors_start;
 	extern uint32_t vectors_end;
-
-	uint32_t *vectors_src = ...;	/* See vector.S to get symbols names*/
-	uint32_t *vectors_dst = ...;
+	uint32_t *vectors_src = &vectors_start;
+	uint32_t *vectors_dst = (uint32_t *) 0;
 
 	while (vectors_src < &vectors_end)
-		... = ...;
+		*vectors_dst++ = *vectors_src++;
 }
 /*
  * Turn off interrupts in the ARM processor
  */
-void disable_interrupts(void)
-{
+void disable_interrupts(void) {
 	int status = 0b11010011;
 	asm("msr cpsr, %[ps]" : : [ps]"r"(status));
 }
 /*
  * Turn on interrupts in the ARM processor
  */
-void enable_interrupts(void)
-{
+void enable_interrupts(void) {
 	int status = 0b01010011;
 	asm("msr cpsr, %[ps]" : : [ps]"r"(status));
 }
-/*
- * Configure interrupt for UART0
- */
-void config_interrupt_UART (void)
-{
+
+void config_interrupt_UART(void) {
 	GICD_ISENABLER1 = 0xffffffff;
 	GICD_IPTR = 0x1;
 }
@@ -92,24 +91,18 @@ void config_interrupt_UART (void)
 /*
  * Configure the Generic Interrupt Controller (GIC)
  */
-void config_GIC(void)
-{
+void config_GIC(void) {
 	config_interrupt_UART();    // configure the UART0 interrupt (44)
-	GICC_PMR = 0xFF;			// Set Interrupt Priority Mask Register (ICCPMR). Enable all priorities
-	GICC_CTRL = 1;				// Set the enable in the CPU Interface Control Register (ICCICR)
-	GICD_CTRL = 1;				// Set the enable in the Distributor Control Register (ICDDCR)
+	GICC_PMR = 0xFF; // Set Interrupt Priority Mask Register (ICCPMR). Enable all priorities
+	GICC_CTRL = 1; // Set the enable in the CPU Interface Control Register (ICCICR)
+	GICD_CTRL = 1; // Set the enable in the Distributor Control Register (ICDDCR)
 }
-/*
- * Unmask the receive interrupt
- */
-void config_UART(void)
-{
-	UART0_IMSC = ...;
+void config_UART(void) {
+	UART0_IMSC = 1 << 4;		// Enable RX for UART0
+	...;						// Enable RX for UART1
 }
 
-void main(void)
-{
-	int seed = 57;
+void main(void) {
 
 	disable_interrupts();   	// disable interrupts in the processor
 	config_GIC();              	// configure the general interrupt controller
@@ -117,8 +110,6 @@ void main(void)
 	enable_interrupts();    	// enable interrupts in the processor
 
 	while (1) {
-		seed = seed * 0x343fd + 0x269ec3;
-		if ( ((seed >> 0x4) & 0x7ffffff) == 2 )
-			UART0_DR = '.';
 	}
+
 }
